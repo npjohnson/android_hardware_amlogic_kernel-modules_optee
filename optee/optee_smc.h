df6af1c78539bbee431f1319dd07c273a33d2c89
rui guo <rui.guo@amlogic.com>
2022-06-24 17:18:22 +0800

    linuxdriver: fix typo check [1/1]
    PD#SWPL-86276
    
    Problem:
    typo check
    
    Solution:
    Correct word
    
    Verify:
    Android R
    
    Signed-off-by: rui guo <rui.guo@amlogic.com>
    Change-Id: I171c4cc2c6815452ef30eee304d1f272f07ecdbe

--- optee/optee_smc.h
+++ optee/optee_smc.h
@@ -153,7 +153,7 @@ struct optee_smc_calls_revision_result {
  *					optee_msg_arg.
  * OPTEE_SMC_RETURN_ETHREAD_LIMIT	Number of Trusted OS threads exceeded,
  *					try again later.
- * OPTEE_SMC_RETURN_EBADADDR		Bad physcial pointer to struct
+ * OPTEE_SMC_RETURN_EBADADDR		Bad physicial pointer to struct
  *					optee_msg_arg.
  * OPTEE_SMC_RETURN_EBADCMD		Bad/unknown cmd in struct optee_msg_arg
  * OPTEE_SMC_RETURN_IS_RPC()		Call suspended by RPC call to normal
